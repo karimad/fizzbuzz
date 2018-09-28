b970ca5cd2e45b14386b46c10c28be2dfa87a084
karimad <kmehaleb@gmail.com>
2018-09-28 19:02:35 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,4 @@
-fizzbuzzIt = (N) =>[...Array(N).keys()].map(i => (i+1)%3===0?(i+1)%5===0?'fizzbuzz':'fizz':(i+1)%5==0?'buzz':(i+1)).join(' ')
+fizzbuzzIt = (N) =>[...Array(N).keys()].map(i => (i+1)%3===0?(i+1)%5===0?'fizzbuzz':'fizz':(i+1)%5===0?'buzz':(i+1)).join(' ')
 
 module.exports = {
 	fizzbuzzIt
